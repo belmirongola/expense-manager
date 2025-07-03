1a975eeb6e032ef0d661c3734af9e57500274c22
Belmiro Ngola <70179163+belmirongola@users.noreply.github.com>
2025-07-03 22:20:14 +0100

    ⚛️ Add main App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseManager from './components/ExpenseManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseManager />
+    </div>
+  );
+}
+
+export default App;
